072c0a4dab665a2ccf5b919b226e2dbc2435b8eb
Keshav Mishra <47028747+mishrakeshav@users.noreply.github.com>
2020-04-22 20:59:32 +0530

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -6,5 +6,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "established.sm@gmail.com"
-    MAIL_PASSWORD = 'uflzsyfssoswhilv'
+    MAIL_USERNAME = ""
+    MAIL_PASSWORD = ''
